718a86f3556753ea42762baa2f66b0baeecb5647
Arjunsinh Jadeja <mrarjunsinh@gmail.com>
2023-04-18 09:42:29 +0200

    MessageWdget: Nullable clipboard response.

--- packages/stream_chat_flutter/lib/src/message_widget/message_widget.dart
+++ packages/stream_chat_flutter/lib/src/message_widget/message_widget.dart
@@ -998,7 +998,7 @@ class _StreamMessageWidgetState extends State<StreamMessageWidget>
           title: Text(context.translations.copyMessageLabel),
           onClick: () {
             Navigator.of(context, rootNavigator: true).pop();
-            Clipboard.setData(ClipboardData(text: widget.message.text));
+            Clipboard.setData(ClipboardData(text: widget.message.text ?? ''));
           },
         ),
       if (shouldShowEditAction) ...[
